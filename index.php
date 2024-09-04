abcf87d88ff7e5e5fbcfce49ac6fb607d2b4b502
Suraj Raya <surraya58@gmail.com>
2024-09-04 10:13:29 +0200

    final

--- index.php
+++ index.php
@@ -2,9 +2,25 @@
 
 require_once __DIR__ . '/inc/db-connect.php';
 require_once __DIR__ . '/inc/functions.php';
-//test
-$stmt = $pdo->prepare('SELECT * FROM `entries`');
+
+$perPage = 15;
+$currentPage = 1;
+if (isset($_GET['page'])) {
+    $currentPage = @(int) $_GET['page'];
+    if ($currentPage <= 0) {
+        $currentPage = 1;
+    }
+}
+
+
+$stmtCount = $pdo->prepare('SELECT COUNT(*) AS `count` FROM `entries`');
+$stmtCount->execute();
+$countTotal = $stmtCount->fetch(PDO::FETCH_ASSOC)['count'];
+
+$stmt = $pdo->prepare('SELECT * FROM `entries` ORDER BY `id` DESC LIMIT :offset, :perPage');
+$stmt->bindValue('perPage', $perPage, PDO::PARAM_INT);
+$stmt->bindValue('offset', ($currentPage - 1) * $perPage, PDO::PARAM_INT);
 $stmt->execute();
 $entries = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-require __DIR__ . '/views/index.view.php';  
+require __DIR__ . '/views/index.view.php';
